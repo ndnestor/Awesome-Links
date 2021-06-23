7a325929abd653ca9d7c4c5edccab8f82e333e4e
ndnestor <Nathan.nestor@outlook.com>
2021-06-23 14:35:57 -0400

    Implemented new logger

--- main.js
+++ main.js
@@ -1,43 +1,55 @@
+// Module imports
 const Express = require('express');
 
+// Script imports
 const airtableInterface = require('./airtable-intereface.js');
+const logger = require('./global-logger.js');
 
+// Other variable declarations
 const app = Express();
 const port = 3000;
 
-// Set up server
+
+// -- SERVER SETUP -- //
+
+// Initialization
+airtableInterface.updateRecords('Employees');
+
+// Allow connections to the server
 app.listen(port, () => {
-    console.log("[========== The server is listening ==========]");
-    airtableInterface.updateRecords('Employees');
+    logger.info(`[====== The server is listening on port ${port} ======]`);
 });
+
 
 // -- REQUEST HANDLING -- //
 
 // Test endpoint to see if the server is running
 // May be changed to serve a different function later
 app.all('/', async(req, res) => {
-    console.log("Request on root was made");
+    logger.info("Request on root was made");
     res.send("The server is running");
 });
 
 // Updates the record cache
 app.get('/update-records', async(req, res) => {
-    console.log('/update-records');
+    logger.info('Request on /update-records was made');
     airtableInterface.updateRecords('Employees').then((records) => {
+        logger.info(`Sending response`);
         res.send(records);
     }).catch((error) => {
-        console.log(error);
+        // TODO: Send error codes and log it
         res.end();
     });
 });
 
 // Searches for records in a table with a given
 app.get('/search-records', async(req, res) => {
-    console.log('/search-records');
+    logger.info('Request on /search-records was made');
     airtableInterface.searchInField('Employees', 'First Name', 'Nathan', true).then((records) => {
+        logger.info(`Sending response`);
         res.send(records);
     }).catch((error) => {
-        console.log(error);
+        // TODO: Send error codes and log it
         res.end();
     });
 });
