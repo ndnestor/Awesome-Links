c86e7006defad8e23a175dde8baad88d46554de8
ndnestor <Nathan.nestor@outlook.com>
2021-06-24 11:39:11 -0400

    Added log file functionality

--- global-logger.js
+++ global-logger.js
@@ -1,15 +1,49 @@
 // Module imports
 const Logger = require('js-logger');
+const Moment = require('moment');
+const FS = require('fs');
+
+// Other variable declarations
+const logFilePath = `./logs/${Moment().format('[YYYY-MM-DD] HH-mm-ss ZZ')} Awesome Links.log`;
+const writeFileInterval = 1000; // In ms
+const writeFileBuffer = [];
+let writeFileTimerIsActive; //? A bit verbose, consider renaming
+const consoleHandler = Logger.createDefaultHandler();
 
 // Initialization
 Logger.setLevel(Logger.DEBUG);
-const consoleHandler = Logger.createDefaultHandler();
 Logger.setHandler((messages, context) => {
     // Send message to console
     consoleHandler(messages, context);
 
-    // TODO: Write log to log file
+    // Add to write buffer to be written to a file later
+    writeFileBuffer.push(messages);
+    if(!writeFileTimerIsActive) {
+        setTimeout(writeBufferToLogFile, writeFileInterval);
+        writeFileTimerIsActive = true;
+    }
+
 });
+
+// Writes the messages in writeFileBuffer to a log file
+function writeBufferToLogFile() {
+
+    // Loop through messages and concatenate them
+    let messageToWrite = '';
+    writeFileBuffer.forEach((message) => {
+        messageToWrite += message[0] + '\n';
+    });
+
+    // Write message to log file
+    FS.appendFile(logFilePath, messageToWrite, { flag: 'a' }, (error) => {
+        if(error) {
+            console.error(`Failed to write log message to file with the following error:\n${error}`);
+        }
+    });
+
+    // Clear buffer
+    writeFileBuffer.length = 0;
+}
 
 // TODO: Determine some way to handle verbose messages
 const methods = {
