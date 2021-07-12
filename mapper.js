ea6685a320f23d1c87764a12712a27f2ab0ac441
ndnestor <nathan.nestor@rutgers.edu>
2021-07-12 12:34:37 -0400

    Separated map URL into parts and fixed test coords

--- mapper.js
+++ mapper.js
@@ -9,7 +9,11 @@ const methods = {
         logger.info('Getting static map');
         return new Promise((resolve, reject) => {
             try {
-                const mapUrl = `https://api.mapbox.com/styles/v1/nathanawesomeinc/ckqtmukm70m0417mu72g1yeee/static/pin-s-1+000(40.7,-74)/[-116.4159,29.7115,-78.9931,47.8859]/709x426?access_token=pk.eyJ1IjoibmF0aGFuYXdlc29tZWluYyIsImEiOiJja3F0bW9jMnkyNmdoMnZtejNjMTg0czRyIn0.x6imIZ-pCiJaIOMX3SdoQg`
+                const mapStyleUrl = 'https://api.mapbox.com/styles/v1/nathanawesomeinc/ckqtmukm70m0417mu72g1yeee/static/';
+                const markerPath = 'pin-l-1+000(-74.00712,40.71455)/';
+                const mapBoundsPath = '[-128.6095,21.4392,-60.6592,54.0095]/800x500?access_token=pk.eyJ1IjoibmF0aGFuYXdlc29tZWluYyIsImEiOiJja3F0bW9jMnkyNmdoMnZtejNjMTg0czRyIn0.x6imIZ-pCiJaIOMX3SdoQg';
+                const mapUrl = mapStyleUrl + markerPath + mapBoundsPath;
+                //const mapUrl = `https://api.mapbox.com/styles/v1/nathanawesomeinc/ckqtmukm70m0417mu72g1yeee/static/pin-l-1+000(-74.00712,40.71455)/[-128.6095,21.4392,-60.6592,54.0095]/800x500?access_token=pk.eyJ1IjoibmF0aGFuYXdlc29tZWluYyIsImEiOiJja3F0bW9jMnkyNmdoMnZtejNjMTg0czRyIn0.x6imIZ-pCiJaIOMX3SdoQg`
 
                 https.get(mapUrl, (res) => {
                     logger.info(`Static map response has status code "${res.statusCode}"`);
