4b9259310f0560308f39a9444d647bad0c258f3b
ndnestor <Nathan.nestor@outlook.com>
2021-08-05 15:08:14 -0400

    Added stop command

--- console-interface.ts
+++ console-interface.ts
@@ -31,6 +31,10 @@ function handleConsoleCmd(command) {
         case 'ram':
             response += `${memoryUsage().rss / 1000000} megabytes of physical RAM are being used`;
             break;
+        case 'stop':
+            response += 'Stopping process...'
+            process.kill(process.pid, 'SIGTERM');
+            break;
         default:
             response += 'Command not found';
     }
